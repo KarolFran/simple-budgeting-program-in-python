6231dbf00f9e482e5d398878d8789a557df75b2c
KarolFran <49308325+KarolFran@users.noreply.github.com>
2021-09-29 13:59:46 +0200

    Delete expense_tracker.py

--- expense_tracker.py
+++
@@ -1,6 +0,0 @@
-budget = float(input("Enter your budget: "))
-while True:
-    spending = float(input("Enter your spending amount: "))
-    budget -= spending
-    print("Budget after spendings is:",budget)
-    
